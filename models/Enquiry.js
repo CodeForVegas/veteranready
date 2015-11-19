22509ab6f3404d1fa29b24abba975878bbefc90f
bgrove1974 <bgroveben@gmail.com>
2015-11-18 16:08:25 -0800

    Add basic email address validation to Enquiry model

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -13,7 +13,7 @@ var Enquiry = new keystone.List('Enquiry', {
 
 Enquiry.add({
 	name: { type: Types.Name, required: true },
-	email: { type: Types.Email, required: true },
+	email: { type: Types.Email, required: true, match: /.+@.+\..+/, lowercase: true },
 	phone: { type: String },
 	enquiryType: { type: Types.Select, options: [
 		{ value: 'message', label: 'Just leaving a message' },
