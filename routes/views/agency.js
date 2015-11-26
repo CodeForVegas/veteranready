e22a06983a2aae38068fc3fc5024753c1d282238
bgrove1974 <bgroveben@gmail.com>
2015-11-26 08:47:02 -0800

    Tell Agency model to register representative and display imagein view

--- routes/views/agency.js
+++ routes/views/agency.js
@@ -1,14 +1,14 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-	
+
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	
+
 	// Set locals
 	locals.section = 'agency';
 	locals.filters = {
-		post: req.params.post
+		agency: req.params.agency
 	};
 	locals.data = {
 		agency: []
@@ -16,19 +16,19 @@ exports = module.exports = function(req, res) {
 	
 	// Load the current post
 	view.on('init', function(next) {
-		
+
 		var q = keystone.list('Agency').model.find({
       approved: true
 		})
-		
+
 		q.exec(function(err, result) {
 			locals.data.agency = result;
 			next(err);
 		});
-		
+
 	});
-	
+
 	// Render the view
 	view.render('agency');
-	
+
 };
