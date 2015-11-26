c4cb4065f5320fbcc2283617a4fab5db1930fefa
bgrove1974 <bgroveben@gmail.com>
2015-11-26 10:36:40 -0800

    Add topic to Agency model and view

--- models/Agency.js
+++ models/Agency.js
@@ -9,7 +9,8 @@ Agency.add({
   name: { type: String, required: true, index: true },
   email: { type: Types.Email, initial: true, required: true, index: true },
   representative: { type: Types.CloudinaryImage, required: false, index: false },
-  video: { type: Types.Url, required: false, index: false },
+  video: { type: Types.Url, initial: true, required: false, index: false },
+  topic: { type: Types.Select, options: 'All, Education, Employment, Health and Welfare, Housing', default: 'All', initial: true, required: true, index: true },
   approved: { type: Types.Boolean, initial: true, index: true },
 });
 
@@ -22,5 +23,5 @@ Agency.schema.pre('save', function(next) {
   next();
 });
 
-Agency.defaultColumns = 'name, email, approved';
+Agency.defaultColumns = 'name, email, topic, approved';
 Agency.register();
