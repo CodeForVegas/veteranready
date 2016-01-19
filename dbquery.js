ef6ba346125eb32e36fd40319ca86cadc1fe5c1b
bgrove1974 <bgroveben@gmail.com>
2016-01-19 01:36:57 -0800

    Add query for finding topic

--- dbquery.js
+++ dbquery.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/veteran_ready';
 
+
 // Query for all documents in a collection:
 // Find all entries in agencies collection in veteran_ready db:
 var findAgencies = function(db, callback) {
@@ -20,6 +21,28 @@ var findAgencies = function(db, callback) {
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   findAgencies(db, function() {
+    db.close();
+  });
+});
+
+
+// Query by a Top Level Field:
+// The following operation finds documents in the agencies collection whose topic field equals "Employment":
+var findTopic = function(db, callback) {
+  var cursor = db.collection('agencies').find( { "topic": "Employment" } );
+  cursor.each(function(err, doc) {
+    assert.equal(err, null);
+    if (doc != null) {
+      console.log(doc);
+    } else {
+      callback();
+    }
+  });
+};
+// Call the findTopic function:
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  findTopic(db, function() {
     db.close();
   });
 });
