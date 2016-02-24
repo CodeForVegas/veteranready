fb1504cdc754afc807250de8276bf7223cce5d4a
bgrove1974 <bgroveben@gmail.com>
2016-02-24 03:15:02 -0800

    Another google image search clone that works

--- public/js/grid.js
+++ public/js/grid.js
@@ -1,8 +1,75 @@
+/* https://codepen.io/gabrieleromanato/pen/dDyzH */
+(function( $ ) {
+	$.imagePreview = function( element ) {
+		this.$element = $( element );
+		this.init();
+	};
+
+	$.imagePreview.prototype = {
+		init: function() {
+			this.$triggers = this.$element.find( ".image-link" );
+			this.$closeLinks = this.$element.find( ".image-details-close" );
+
+			this.open();
+			this.close();
+		},
+
+		_getContent: function( element ) {
+			var $parent = element.parent(),
+				title = $parent.data( "title" ),
+				desc = $parent.data( "desc" ),
+				html = element.html();
+
+				return {
+					title: title,
+					desc: desc,
+					html: html
+				}
+		},
+
+		open: function() {
+			var self = this;
+			self.$triggers.on( "click", function( e ) {
+				e.preventDefault();
+				var $a = $( this ),
+					content = self._getContent( $a ),
+					$li = $a.parents( "li" ),
+					$details = $( ".image-details", $li ),
+					$contentImage = $( ".image", $details ),
+					$detailsTitle = $( ".image-details-title", $details ),
+					$detailsText = $( ".image-details-text", $details );
+
+					$contentImage.html( content.html );
+					$detailsTitle.text( content.title );
+					$detailsText.text( content.desc );
+
+					self.$element.find( ".image-details" ).slideUp( "fast" );
+					$details.slideDown( "fast" );
+
+			});
+		},
+		close: function() {
+			this.$closeLinks.on( "click", function( e ) {
+				e.preventDefault();
+				$( this ).parent().slideUp( "fast" );
+
+			});
+		}
+	};
+
+	$(function() {
+		var preview = new $.imagePreview( "#image-wrapper" );
+
+	});
+
+})( jQuery );
+
 /* http://codepen.io/SitePoint/pen/EabNrX */
 
 // Demo by Dan Rose.
 // See: http://www.sitepoint.com/recreating-google-images-search-layout-css
 
+/*
 // dynamically create loads of image cells
 function cells(count) {
   if (typeof count !== 'number' || count > 99) return false;
@@ -52,6 +119,8 @@ $cell.find('.expand__close').click(function(){
 
   $thisCell.removeClass('is-expanded').addClass('is-collapsed');
 });
+*/
+
 
 /*
 * debouncedresize: special jQuery event that happens once after a window resize
