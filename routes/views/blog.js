2c3424a6ca040b60bae4cb2a1bcceef68d7b6ed2
bgrove1974 <bgroveben@gmail.com>
2016-03-31 01:32:38 -0700

    Return blog.js and blog.hbs to original state

--- routes/views/blog.js
+++ routes/views/blog.js
@@ -1,45 +1,4 @@
 var keystone = require('keystone');
-
-exports = module.exports = function(req, res) {
-
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
-
-	// Set locals
-	locals.section = 'blog';
-	locals.filters = {
-		agency: req.params.agency
-	};
-	locals.data = {
-		agencies: []
-	};
-
-	// Load the current agency
-	view.on('init', function(next) {
-
-		var q = keystone.list('Agency').model.find({
-      approved: true
-		})
-
-		q.exec(function(err, result) {
-			locals.data.agency = result;
-			next(err);
-		});
-
-	});
-
-	// Render the view
-	view.render('blog');
-
-};
-
-// Original code is below
-/*
-=======
-/* Original code below
-
->>>>>>> thumbnailGrid
-var keystone = require('keystone');
 var async = require('async');
 
 exports = module.exports = function(req, res) {
@@ -125,4 +84,3 @@ exports = module.exports = function(req, res) {
 	view.render('blog');
 
 };
-*/
