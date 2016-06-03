00b3a3657e039eebd682fdd489a1c1b2858a26f1
bgrove1974 <bgroveben@gmail.com>
2016-06-03 03:54:33 -0700

    Fix problematic phone validation for users' contact info

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -18,7 +18,8 @@ Enquiry.add({
          { value: 'representative', label: 'Representing a Business or Organization'}
 	] },
   email: { type: Types.Email, required: true, match: /.+@.+\..+/, lowercase: true },
-	phone: { type: String, match: /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/ },
+	phone: { type: String},
+	// match: /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/ },
 	enquiryType: { type: Types.Select, options: [
 		{ value: 'services', label: 'Our Services' },
 		{ value: 'events', label: 'Post an Event' },
